d192036896c6ba8504fd69d7c7b9f12ae1033a8f
James Cryer <j.r.cryer@gmail.com>
2011-11-16 23:55:23 +0000

    Moving index to route file

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,55 @@
 /*
  * GET Individual Video
  */
+var jsdom = require('jsdom')
+, request = require('request')
+, url = require('url');
 
+exports.list  = function (req, res) {
+    //Tell the request that we want to fetch youtube.com, send the results to a callback function
+    request({
+        uri: 'http://youtube.com'
+    }, function (err, response, body) {
+        var self   = this;
+        var videos = []; 
+
+        //Just a basic error check
+        if (err && response.statusCode !== 200) {
+            console.log('Request error.');
+        }
+ 
+        //Send the body param as the HTML code we will parse in jsdom
+        //also tell jsdom to attach jQuery in the scripts
+        jsdom.env({
+            html: body,
+            scripts: ['http://code.jquery.com/jquery-1.6.min.js']
+        }, function (err, window) {
+            var $ = window.jQuery,
+                $body = $('body'),
+                $videos = $body.find('.video-entry');
+ 
+            $videos.each(function (i, item) { 
+                var $a = $(item).children('a'),
+                    $title = $(item).find('.video-title .video-long-title').text(),
+                    $time = $a.find('.video-time').text(),
+                    $img = $a.find('span.clip img'); //thumbnail
+ 
+                videos.push({
+                    href: $a.attr('href'),
+                    title: $title.trim(),
+                    time: $time, 
+                    thumbnail: $img.attr('data-thumb') ? $img.attr('data-thumb') : $img.attr('src'),
+                    urlObj: url.parse($a.attr('href'), true) //parse our URL and the query string as well
+                });
+            });
+ 
+	    res.render('list', {
+                title: 'NodeTube',
+                items: videos
+	    }); 
+        });
+    });
+};
 exports.watch = function (req, res) {
    res.render('video', {
       title: 'Watch',
